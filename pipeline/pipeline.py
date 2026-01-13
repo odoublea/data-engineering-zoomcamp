f9467c0a1e739d28f1f02879eaa1e977db218d08
Abdulqudus Oladega <oladegakenny@gmail.com>
2026-01-13 17:50:21 +0000

    updated pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,4 +1,12 @@
 import sys
 import pandas as pd
 
-print("Hello")
+print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
+
+df = pd.DataFrame({"Item": [1, 2], "Price": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
